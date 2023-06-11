d97f0885563d780c80e7ad449816b89860d987f7
dus532 <dus532@naver.com>
2023-06-12 04:01:55 +0900

    Feat: 컬러 모드 도입

--- src/components/organisms/Header.tsx
+++ src/components/organisms/Header.tsx
@@ -1,7 +1,10 @@
+'use client';
+
 import Container from '@/components/templates/Container';
 import React from 'react';
 import styles from './Header.module.scss';
-import LOGO from '@/assets/svgs/logo_full.svg';
+import LogoFullDark from '@/assets/svgs/LogoFullDark.svg';
+import LogoFullLight from '@/assets/svgs/LogoFullLight.svg';
 import Link from 'next/link';
 import HeaderMenu from '@/components/organisms/HeaderMenu';
 
@@ -11,8 +14,11 @@ export default function Header() {
       <Container>
         <div className={styles.HeaderContainer}>
           <Link href='/'>
-            <div className={styles.Logo}>
-              <LOGO />
+            <div className={`${styles.Logo} ${styles.LogoDark}`}>
+              <LogoFullDark />
+            </div>
+            <div className={`${styles.Logo} ${styles.LogoLight}`}>
+              <LogoFullLight />
             </div>
           </Link>
           <div className={styles.Menu}>
